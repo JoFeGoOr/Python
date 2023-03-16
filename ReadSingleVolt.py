439cc7399c1334a54308589dffbe2d4f2b8fa6aa
JoFeGoOr <jorge.gonzalez.o@mail.pucv.cl>
2023-03-16 10:50:03 -0300

    avance conexion

--- ReadSingleVolt.py
+++ ReadSingleVolt.py
@@ -1,21 +1,27 @@
-from uldaq import (get_daq_device_inventory,DaqDevice,InterfaceType)
+from uldaq import (get_daq_device_inventory,DaqDevice,InterfaceType,AiInputMode)
 
 def main():
-    interface_type = InterfaceType.ANY
+
+    high_channel = 3
+    range_index = 1
+
 
     try:
-        devices = get_daq_device_inventory(interface_type)
+        # Obtenemos una lista de objetos(dispositivos) que pueden ser usados como DaqDevice
+        devices = get_daq_device_inventory(InterfaceType.ANY)
         number_of_devices = len(devices)
 
+        # Verificamos si existe algun dispositivo conectado
         if number_of_devices == 0:
             raise RuntimeError('Error: No dispositivo DAQ encontrado')
         
+        # Describimos la cantidad de objetos(dispositivos) encontrados y el detallle correspondiente
         print(number_of_devices, 'Dispositivo(s) DAQ encontrado(s):')
-
         for i in range(number_of_devices):
             print('  [', i, '] ', devices[i].product_name, ' (',
                   devices[i].unique_id, ')', sep='')
-            
+        
+        # Si existe solo 1, su indice es automaticamente 0, si existe mas de 1, se debe verificar con que dispositivo queremos interactuar
         if number_of_devices != 1:
             descriptor_index = input('\nSeleccione un dispositivo DAQ, ingrese un numero'
                                  + ' entre 0 y '
@@ -29,15 +35,30 @@ def main():
         # Crea el dispositivo DAQ desde el descriptor con el indice especificado.
         daq_device = DaqDevice(devices[descriptor_index])
 
-        # Obtiene el objeto AIDevice y verifica si es valido.
+        # Obtiene el objeto AIDevice(Subsistema de entradas analogicas) y verifica si es valido.
         ai_device = daq_device.get_ai_device()
         if ai_device is None:
             raise RuntimeError('Error: El dispositivo DAQ no soporta '
                                'entradas analogicas')
         
-        # Establecemos la conexion con el dispositivo DAQ
+        # Establecemos la conexion con el dispositivo DAQ, 2 flash de led indica que
         descriptor = daq_device.get_descriptor()
         print('\nConectando con', descriptor.dev_string, '- por favor espere')
+        daq_device.connect(connection_code=0)
+        if daq_device.is_connected() == True:
+            daq_device.flash_led(3)
+        else:
+            raise RuntimeError('Error: No se pudo hacer conexion con el dispositivo')
+
+        ai_info = ai_device.get_info()
+
+        # La entrada por defecto es SINGLE_ENDED.
+        input_mode = AiInputMode.SINGLE_ENDED
+
+        ranges = ai_info.get_ranges(input_mode)
+        #for i in range(len(ranges)):
+        #    print(ranges[i])
+
 
     except RuntimeError as error:
         print('\n', error)
