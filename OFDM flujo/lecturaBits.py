ab5a344506e2da89a02926ccb9824dbaa17ed4e9
JoFeGoOr <jorge.gonzalez.o@mail.pucv.cl>
2023-04-03 11:16:40 -0400

    lectura bits

--- OFDM flujo/lecturaBits.py
+++ OFDM flujo/lecturaBits.py
@@ -1,3 +1,10 @@
 import numpy
 
-f = numpy.fromfile('bit-in.txt')
+pathin = "/home/uno/Python/OFDM flujo/bit-in.txt"
+pathout = "/home/uno/Python/OFDM flujo/bit-out.txt"
+
+fin = numpy.fromfile(pathin,numpy.byte)
+fout = numpy.fromfile(pathout,numpy.byte)
+
+print("largo de bits entrantes : ",len(fin),sep="")
+print("largo de bits salientes : ",len(fout),sep="")
